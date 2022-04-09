9d7e136657ce613c21c3bee98022f6d53d687d6d
hackertyus <88194972+hackertyus@users.noreply.github.com>
2022-04-10 02:03:18 +0300

    Update yt_dlp_button.py

--- plugins/yt_dlp_button.py
+++ plugins/yt_dlp_button.py
@@ -296,14 +296,14 @@ async def yt_dlp_call_back(bot, update):
                 BotMention = user["mention"]
                 UserMention = update.from_user.mention
 
-                if PROMO is False:
-                    reply_markup = False
-                elif PROMO is True:
+                if PROMO:
                     caption += Translation.UPLOADER.format(UserMention, BotMention)
                     btn = [[
                         InlineKeyboardButton(f"Uploaded By {user.first_name}", url=f"tg://user?id={user.id}")
                     ]]
                     reply_markup = InlineKeyboardMarkup(btn)
+                else:
+                    reply_markup = False
 
                 try:
                     if tg_send_type == "audio":
